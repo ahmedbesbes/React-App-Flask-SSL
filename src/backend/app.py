3247345de431c9170f182cbc1f8d6766a839be1f
Ahmed Besbes <ahmed.besbes@hotmail.com>
2020-05-31 17:23:12 +0200

    cleaning up

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, Blueprint, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+api = Blueprint('api', __name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World! This app is running with Flask and using an SSL certificate"
+@api.route('/submit', methods=['POST'])
+def handle_submit():
+    if request.method == "POST":
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        job = request.form['job']
+        print(f'first name : {first_name}')
+        print(f'last name : {last_name}')
+        print(f'job : {job}')
 
+        # do your processing logic here.
+
+        return jsonify({
+            "firstName": first_name,
+            "lastName": last_name,
+            "job": job
+        })
+
+
+app.register_blueprint(api, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5050)
---
+++ src/frontend/src/components/BasicForm.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Alert from "@material-ui/lab/Alert";
+
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    margin: 5,
+  },
+}));
+
+export default function BasicForm() {
+  const baseUrl = process.env.REACT_APP_API_URL;
+  const classes = useStyles();
+  const [identity, setIdentity] = useState({ firstName: "", lastName: "" });
+  const [job, setJob] = useState("");
+  const [success, setSuccess] = useState(undefined);
+
+  function handleInputFirstName(e) {
+    setIdentity({ ...identity, firstName: e.target.value });
+  }
+
+  function handleInputLastName(e) {
+    setIdentity({ ...identity, lastName: e.target.value });
+  }
+
+  function handleInputJob(e) {
+    setJob(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const data = new FormData();
+    data.set("firstName", identity.firstName);
+    data.set("lastName", identity.lastName);
+    data.set("job", job);
+
+    axios({
+      method: "post",
+      url: baseUrl + "/submit",
+      data: data,
+    })
+      .then(function (response) {
+        console.log(response);
+        setSuccess(true);
+      })
+      .catch(function (response) {
+        setSuccess(false);
+        console.log(response);
+      });
+  }
+
+  function Alerting() {
+    if (success === true) {
+      return <Alert severity="success">Data sucessfully sent !</Alert>;
+    } else if (success === false) {
+      return <Alert severity="error">Oops. An error just occured!</Alert>;
+    } else {
+      return null;
+    }
+  }
+
+  return (
+    <Container>
+      <form>
+        <Grid container justify="center">
+          <Grid item xs={12} sm={12} md={6} lg={6} className={classes.input}>
+            <TextField
+              required
+              name="firstname"
+              id="firstname"
+              label="Frist name"
+              fullWidth={true}
+              value={identity.firstName}
+              onChange={handleInputFirstName}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={6} lg={6} className={classes.input}>
+            <TextField
+              name="lastname"
+              id="lastname"
+              label="Last name"
+              fullWidth={true}
+              value={identity.lastName}
+              onChange={handleInputLastName}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={6} lg={6} className={classes.input}>
+            <TextField
+              name="job"
+              id="firstname"
+              label="Job"
+              fullWidth={true}
+              value={job}
+              onChange={handleInputJob}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <Typography align="center">
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+              <div style={{ marginTop: 10 }}>
+                <Alerting></Alerting>
+              </div>
+            </Typography>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+}
